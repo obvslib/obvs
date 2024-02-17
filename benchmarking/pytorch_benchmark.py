a8b6c40a465438064be85d568dbc5ff05c4c1067
llinauer <l.linauer@gmx.at>
2024-02-17 09:51:55 +0100

    Rename compile -> compile_model since compile is a python built-in function

--- benchmarking/pytorch_benchmark.py
+++ benchmarking/pytorch_benchmark.py
@@ -5,12 +5,12 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from typing import Tuple
 
 
-def setup(model_id: str, compile=False) -> Tuple[AutoModelForCausalLM, AutoTokenizer]:
+def setup(model_id: str, compile_model: bool = False) -> Tuple[AutoModelForCausalLM, AutoTokenizer]:
     """ Setup model and tokenizer and return them.
         If compile==True -> compile the model with torch.compile"""
 
     model = AutoModelForCausalLM.from_pretrained(model_id)
-    if compile:
+    if compile_model:
         model = torch.compile(model)
 
     tokenizer = AutoTokenizer.from_pretrained(model_id)
@@ -31,11 +31,12 @@ def generate(prompt: str, model: AutoModelForCausalLM, tokenizer: AutoTokenizer)
     return generated_text
 
 
-def run_benchmark(prompt: str, model_id: str = "nickypro/tinyllama-110M") -> Tuple[str, float]:
+def run_benchmark(prompt: str, model_id: str = "nickypro/tinyllama-110M",
+                  compile_model: bool = False) -> Tuple[str, float]:
     """ Setup model and run text generation on the prompt, estimating the average run time.
         Return the generated text and the average runtime """
 
-    model, tokenizer = setup(model_id)
+    model, tokenizer = setup(model_id, compile_model)
 
     # create a Timer object for estimating the runtime
     timer = timeit.Timer(lambda: generate(prompt, model, tokenizer))
