5c5ab7670bcfcbd46ddbfb0757e658946c9189df
llinauer <l.linauer@gmx.at>
2024-02-17 09:41:38 +0100

    Separate the logic blocks in nnsight_benchmark.py into functions setup, generate and run_benchmark
    This is done with the intention of one super-script, that calls the run_benchmark script from all the benchmark scripts

--- benchmarking/nnsight_benchmark.py
+++ benchmarking/nnsight_benchmark.py
@@ -1,14 +1,35 @@
+import timeit
+import custom_timeit
 from nnsight import LanguageModel
-
-llama = "nickypro/tinyllama-110M"
-model = LanguageModel(llama)
-
-prompt = "The quick brown fox jumps over the lazy"
-
-with model.generate(max_new_tokens=10) as generator:
-    with generator.invoke(prompt) as _:
-        pass
+from typing import Tuple
 
 
-output = model.tokenizer.decode(generator.output[0])
-print(output)
+def setup(model_id: str) -> LanguageModel:
+    """ Setup model and return it """
+    return LanguageModel(model_id)
+
+
+def generate(prompt: str, model: LanguageModel) -> str:
+    """ Generate new text from the given prompt and model.
+        Return the generated text as a string """
+
+    with model.generate(max_new_tokens=10) as generator:
+        with generator.invoke(prompt) as _:
+            pass
+
+    # return generated text
+    return model.tokenizer.decode(generator.output[0])
+
+
+def run_benchmark(prompt: str, model_id: str = "nickypro/tinyllama-110M") -> Tuple[str, float]:
+    """ Setup model and run text generation on the prompt, estimating the average run time.
+        Return the generated text and the average runtime """
+
+    model = setup(model_id)
+
+    # create a Timer object for estimating the runtime
+    timer = timeit.Timer(lambda: generate(prompt, model))
+    avg_runtime, generated_text = timer.timeit(number=1)
+    return avg_runtime, generated_text
+
+
