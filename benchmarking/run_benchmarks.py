68d4d6eb79f27431a5f46f28501386c631cd133a
llinauer <l.linauer@gmx.at>
2024-02-17 10:14:15 +0100

    Start working on run_benchmarks.py
    This script runs the text-generation benchmark on nnsight, pytorch w & w/o compiling

---
+++ benchmarking/run_benchmarks.py
@@ -0,0 +1,30 @@
+"""
+run_benchmarks.py
+
+Run the nnsight, pytorch & pytorch_compile benchmarks on a number of prompts
+"""
+
+import csv
+from nnsight_benchmark import run_benchmark as run_nnsight_benchmark
+from pytorch_benchmark import run_benchmark as run_pytorch_benchmark
+
+
+def run_benchmarks(prompt: str, model_id: str, write_to_csv: bool = True):
+    """ Run the nnsight, pytorch & pytorch compile benchmarks on the given prompt.
+        If write_to_csv is True, write the results to a .csv file"""
+
+    results = {}
+
+    nnsight_runtime, nnsight_output = run_nnsight_benchmark(prompt, model_id)
+    pytorch_runtime, pytorch_output = run_pytorch_benchmark(prompt, model_id)
+    pytorch_compile_runtime, pytorch_compile_output = run_pytorch_benchmark(prompt, model_id,
+                                                                            compile_model=True)
+
+    results['nnsight'] = (nnsight_runtime, nnsight_output)
+    results['pytorch'] = (pytorch_runtime, pytorch_output)
+    results['pytorch_compile'] = (pytorch_compile_runtime, pytorch_compile_output)
+    print(results)
+
+
+if __name__ == '__main__':
+    run_benchmarks('The quick brown fox jumps over the lazy', 'nickypro/tinyllama-110M')
