56eb8e53694249145605112fcb561272b737cad4
llinauer <l.linauer@gmx.at>
2024-02-18 08:20:43 +0100

    Rename run_benchmarks -> run_benchmark_on_prompt

--- benchmarking/run_benchmarks.py
+++ benchmarking/run_benchmarks.py
@@ -9,7 +9,7 @@ from nnsight_benchmark import run_benchmark as run_nnsight_benchmark
 from pytorch_benchmark import run_benchmark as run_pytorch_benchmark
 
 
-def run_benchmarks(prompt: str, model_id: str, write_to_csv: bool = True):
+def run_benchmark_on_prompt(prompt: str, model_id: str) -> dict:
     """ Run the nnsight, pytorch & pytorch compile benchmarks on the given prompt.
         If write_to_csv is True, write the results to a .csv file"""
 
@@ -23,8 +23,9 @@ def run_benchmarks(prompt: str, model_id: str, write_to_csv: bool = True):
     results['nnsight'] = (nnsight_runtime, nnsight_output)
     results['pytorch'] = (pytorch_runtime, pytorch_output)
     results['pytorch_compile'] = (pytorch_compile_runtime, pytorch_compile_output)
-    print(results)
+
+    return results
 
 
 if __name__ == '__main__':
-    run_benchmarks('The quick brown fox jumps over the lazy', 'nickypro/tinyllama-110M')
+    run_benchmark_on_prompt('The quick brown fox jumps over the lazy', 'nickypro/tinyllama-110M')
