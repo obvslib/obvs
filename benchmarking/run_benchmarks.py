a13c7532f02c16d7c3cd9ac42e83d0d7dca47afd
llinauer <l.linauer@gmx.at>
2024-02-18 10:53:33 +0100

    Add plot_results function to run_benchmarks.py

--- benchmarking/run_benchmarks.py
+++ benchmarking/run_benchmarks.py
@@ -10,6 +10,7 @@ from nnsight_benchmark import run_benchmark as run_nnsight_benchmark
 from pytorch_benchmark import run_benchmark as run_pytorch_benchmark
 from typing import List, Tuple
 from functools import partial
+import plotly.graph_objs as go
 
 
 def run_benchmark_on_prompt(prompt: str, model_id: str) -> List[Tuple]:
@@ -28,30 +29,29 @@ def run_benchmark_on_prompt(prompt: str, model_id: str) -> List[Tuple]:
     return results
 
 
-def run_benchmarks(prompts: List[str], model_id: str, write_to_csv: bool = True,
+def run_benchmarks(prompt: str, model_id: str, write_to_csv: bool = True,
                    csv_file_name: str = 'python_benchmark_results.csv') -> None:
     """ Run benchmarks for all prompts with model_id. If write_to_csv == True, write the
         results to the given csv file"""
 
-    for i, prompt in enumerate(prompts):
-        prompt_results = run_benchmark_on_prompt(prompt, model_id)
+    prompt_results = run_benchmark_on_prompt(prompt, model_id)
 
-        if write_to_csv:
-            # check if csv file already exists
-            if not Path(csv_file_name).exists():
-                with open(csv_file_name, 'w') as csv_file:
-                    csv_writer = csv.writer(csv_file)
-                    csv_writer.writerow(
-                        ['method', 'model_id', 'prompt', 'avg_runtime', 'std_runtime',
-                         'generated_text']
-                    )
-
-            with open(csv_file_name, 'a') as csv_file:
+    if write_to_csv:
+        # check if csv file already exists
+        if not Path(csv_file_name).exists():
+            with open(csv_file_name, 'w') as csv_file:
                 csv_writer = csv.writer(csv_file)
-                for result in prompt_results:
-                    csv_writer.writerow(
-                        [result[0], model_id, prompt, result[1], result[2], result[3]]
-                    )
+                csv_writer.writerow(
+                    ['method', 'model_id', 'prompt', 'avg_runtime', 'std_runtime',
+                     'generated_text']
+                )
+
+        with open(csv_file_name, 'a') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            for result in prompt_results:
+                csv_writer.writerow(
+                    [result[0], model_id, prompt, result[1], result[2], result[3]]
+                )
 
 
 def read_results_file(results_file: str) -> dict:
@@ -69,18 +69,55 @@ def read_results_file(results_file: str) -> dict:
         # skip header
         next(csv_reader)
         for row in csv_reader:
-            prompt = row[2]
-            results[prompt] = (row[0], row[1], row[3], row[4])
+            results[row[0]] = (row[1], row[2], row[3], row[4])
 
     return results
 
 
-if __name__ == '__main__':
-    benchmark_prompts = ['The capital of France is',
-                         'The quick brown fox jumps over the lazy',
-                         'Midnight had just passed when there came a knock on the door, and',
-                         'In a world where machines think and learn like humans,'
-                         ' there exists a concept known as artificial intelligence',]
+def plot_results(results: dict):
+    """ Plot the results of the benchmarks """
 
-    run_benchmarks(benchmark_prompts, 'nickypro/tinyllama-110M', write_to_csv=True,
+    bar_names = []
+    runtimes_avg = []
+    runtimes_std = []
+    colors = ['blue', 'green', 'red', 'yellow']
+
+    for method, result_tuple in results.items():
+        # get average runtime and std deviations
+        bar_names.append(method)
+        runtimes_avg.append(float(result_tuple[2]))
+        runtimes_std.append(float(result_tuple[3]))
+        model_name = result_tuple[0]
+        prompt = result_tuple[1]
+
+    fig = go.Figure()
+
+    print(bar_names, runtimes_avg, runtimes_std)
+
+    # Add bar trace
+    fig.add_trace(go.Bar(
+        x=bar_names,
+        y=runtimes_avg,
+        error_y=dict(
+            type='data',
+            array=runtimes_std,
+            visible=True
+        ),
+        marker=dict(color=colors)
+    ))
+
+    # Update layout
+    fig.update_layout(
+        title=f'Text Generation Benchmark. Model: "{model_name}", n_new_tokens: 20, Prompt: "{prompt}"',
+        xaxis=dict(title='Method'),
+        yaxis=dict(title='Avg runtimes')
+    )
+
+    fig.show()
+
+
+if __name__ == '__main__':
+    benchmark_prompt = 'The quick brown fox jumps over the lazy'
+
+    run_benchmarks(benchmark_prompt, 'nickypro/tinyllama-110M', write_to_csv=True,
                    csv_file_name='python_benchmarks.csv')
