27c006cdd4fcd8d3833a6cb34047a73b21c2bb94
llinauer <l.linauer@gmx.at>
2024-02-18 09:38:39 +0100

    Adapt run_benchmarks.py to usage of standard deviation

--- benchmarking/run_benchmarks.py
+++ benchmarking/run_benchmarks.py
@@ -9,6 +9,7 @@ from pathlib import Path
 from nnsight_benchmark import run_benchmark as run_nnsight_benchmark
 from pytorch_benchmark import run_benchmark as run_pytorch_benchmark
 from typing import List, Tuple
+from functools import partial
 
 
 def run_benchmark_on_prompt(prompt: str, model_id: str) -> List[Tuple]:
@@ -17,14 +18,12 @@ def run_benchmark_on_prompt(prompt: str, model_id: str) -> List[Tuple]:
 
     results = []
 
-    nnsight_runtime, nnsight_output = run_nnsight_benchmark(prompt, model_id)
-    pytorch_runtime, pytorch_output = run_pytorch_benchmark(prompt, model_id)
-    pytorch_compile_runtime, pytorch_compile_output = run_pytorch_benchmark(prompt, model_id,
-                                                                            compile_model=True)
+    benchmarks_funcs = [('nnsight', run_nnsight_benchmark), ('pytorch', run_pytorch_benchmark),
+                        ('pytorch_compile', partial(run_pytorch_benchmark, compile_model=True))]
 
-    results.append(('nnsight', nnsight_runtime, nnsight_output))
-    results.append(('pytorch', pytorch_runtime, pytorch_output))
-    results.append(('pytorch_compile', pytorch_compile_runtime, pytorch_compile_output))
+    for benchmark_name, benchmark_func in benchmarks_funcs:
+        avg_runtime, std_runtime, output = benchmark_func(prompt, model_id)
+        results.append((benchmark_name, avg_runtime, std_runtime, output))
 
     return results
 
@@ -42,14 +41,20 @@ def run_benchmarks(prompts: List[str], model_id: str, write_to_csv: bool = True,
             if not Path(csv_file_name).exists():
                 with open(csv_file_name, 'w') as csv_file:
                     csv_writer = csv.writer(csv_file)
-                    csv_writer.writerow(['method', 'model_id', 'prompt', 'avg_runtime',
-                                         'generated_text'])
+                    csv_writer.writerow(
+                        ['method', 'model_id', 'prompt', 'avg_runtime', 'std_runtime',
+                         'generated_text']
+                    )
 
             with open(csv_file_name, 'a') as csv_file:
                 csv_writer = csv.writer(csv_file)
-                csv_writer.writerow([prompt_results[0], model_id, prompt, prompt_results[1],
-                                     prompt_results[2]])
+                for result in prompt_results:
+                    csv_writer.writerow([result[0], model_id, prompt, result[1], result[2]])
 
 
 if __name__ == '__main__':
-    run_benchmark_on_prompt('The quick brown fox jumps over the lazy', 'nickypro/tinyllama-110M')
+    benchmark_prompts = ['The quick brown fox jumps over the lazy',
+                         'The capital of France is']
+
+    run_benchmarks(benchmark_prompts, 'nickypro/tinyllama-110M', write_to_csv=True,
+                   csv_file_name='python_benchmarks.csv')
