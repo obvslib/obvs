04a9a5f61d8d329a71640c63197504d8d0ba6bb2
fergus <fergusfettes@gmail.com>
2024-02-24 18:37:37 +0530

    added modal scripts

---
+++ modal_images/gemma.py
@@ -0,0 +1,45 @@
+from modal import Image, Stub, Secret
+
+
+# Define the model names for LLaMA-2, Mistral, and GPT-2
+model_names = {
+    "llamatiny": "TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T",
+    "llama": "meta-llama/Llama-2-13b-hf",
+    "gpt2": "gpt2",
+    "mistral": "mistralai/Mistral-7B-v0.1",
+    "gptj": "EleutherAI/gpt-j-6B",
+    "gemma2": "google/gemma-2b",
+    "gemma7": "google/gemma-7b",
+}
+
+
+# Spec for an image where gptj is cached locally
+def download_model():
+    import os
+    token = os.environ["HUGGINGFACE_TOKEN"].strip()
+    from huggingface_hub import snapshot_download
+    snapshot_download(model_names["gemma7"], token=token, revision="main")
+    snapshot_download(model_names["gemma2"], token=token, revision="main")
+
+
+image = (
+    Image.from_registry(
+        "nvidia/cuda:12.1.1-devel-ubuntu22.04", add_python="3.11"
+    )
+    .apt_install("git")
+    .pip_install(
+        "git+https://github.com/fergusfettes/obvs.git@modal",
+    )
+    .run_function(
+        download_model,
+        secrets=[Secret.from_name("my-huggingface-secret")],
+    )
+)
+
+
+stub = Stub(image=image, name="gptj")
+
+
+@stub.local_entrypoint()
+def main():
+    print("building the model...")
---
+++ modal_images/token_identity.py
@@ -0,0 +1,141 @@
+from __future__ import annotations
+
+import time
+
+import numpy as np
+import torch
+from tqdm import tqdm
+from pathlib import Path
+
+from obvspython.logging import logger
+from obvspython.patchscope import Patchscope, SourceContext, TargetContext
+from obvspython.vis import create_heatmap, plot_surprisal
+
+from modal import Stub, gpu, method
+from modal_images.gemma import image
+
+
+# Define the model names for LLaMA-2, Mistral, and GPT-2
+model_names = {
+    "llamatiny": "TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T",
+    "llama": "meta-llama/Llama-2-13b-hf",
+    "gpt2": "gpt2",
+    "mamba": "MrGonao/delphi-mamba-100k",
+    "mistral": "mistralai/Mistral-7B-v0.1",
+    "gptj": "EleutherAI/gpt-j-6B",
+    "gemma": "google/gemma-2b"
+}
+
+
+stub = Stub(image=image, name="token_identity")
+
+
+@stub.cls(
+    gpu=gpu.A100(memory=80, count=1),
+    timeout=60 * 20,
+    container_idle_timeout=60 * 5,
+)
+class Runner:
+    @method()
+    def run(patchscope, target_tokens, values):
+        source_layers = list(range(patchscope.n_layers))
+        target_layers = list(range(patchscope.n_layers))
+        iterations = len(source_layers) * len(target_layers)
+
+        with tqdm(total=iterations) as pbar:
+            outputs = patchscope.over_pairs(source_layers, target_layers)
+            pbar.update(1)
+
+        # with tqdm(total=iterations) as pbar:
+        #     outputs = patchscope.over(source_layers, target_layers)
+        #     pbar.update(1)
+
+        logger.info("Computing surprisal")
+        target_output = 0
+
+        for i in source_layers:
+            # Get the output of the run
+            probs = torch.softmax(outputs[i][target_output], dim=-1)
+            values[i] = patchscope.compute_surprisal(probs[-1], target_tokens)
+
+        # for i in source_layers:
+        #     for j in target_layers:
+        #         # Get the output of the run
+        #         probs = torch.softmax(outputs[i][j][target_output].value, dim=-1)
+        #         values[i, j] = patchscope.compute_surprisal(probs[-1], target_tokens)
+
+        logger.info("Done")
+
+        return source_layers, target_layers, values, outputs
+
+
+@stub.local_entrypoint()
+def main(
+    word: str = "boat",
+    model: str = "gemma",
+    prompt: str = "if its on the road, its a car. if its in the air, its a plane. if its on the sea, its a",
+):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Generating definition for word: {word} using model: {model}")
+    if model in model_names:
+        model = model_names[model]
+
+    model_name = model.replace("/", "-")
+    filename = f"{model_name}_{word}"
+
+    # Setup source and target context with the simplest configuration
+    source_context = SourceContext(
+        prompt=prompt,  # Example input text
+        model_name=model,  # Model name
+        position=-1,
+        device=device,
+    )
+
+    target_context = TargetContext.from_source(source_context)
+    target_context.prompt = (
+        "bat is bat; 135 is 135; hello is hello; black is black; shoe is shoe; x is"
+    )
+    target_context.max_new_tokens = 1
+    patchscope = Patchscope(source=source_context, target=target_context)
+
+    try:
+        patchscope.source.position, target_tokens = patchscope.source_position_tokens(word)
+        patchscope.target.position, _ = patchscope.target_position_tokens("X")
+
+        assert (
+            patchscope.source_words[patchscope.source.position].strip() == word
+        ), patchscope.source_words[patchscope.source.position]
+        assert (
+            patchscope.target_words[patchscope.target.position].strip() == "X"
+        ), patchscope.target_words[patchscope.target.position]
+
+    except ValueError:
+        target_tokens = patchscope.tokenizer.encode(" boat", add_special_tokens=False)
+        patchscope.source.position = -1
+        patchscope.target.position = -1
+
+    if Path(f"scripts/{filename}.npy").exists():
+        values = np.load(f"scripts/{filename}.npy")
+    else:
+        values = np.zeros((patchscope.n_layers_source, patchscope.n_layers_target))
+
+    start = time.time()
+    runner = Runner()
+    source_layers, target_layers, values, outputs = runner.run.remote(patchscope, target_tokens, values)
+    print(f"Elapsed time: {time.time() - start:.2f}s. Layers: {source_layers}, {target_layers}")
+
+    # Save the values to a file
+    np.save(f"scripts/{filename}.npy", values)
+
+    fig = plot_surprisal(source_layers, [value[0] for value in values], title=f"Token Identity: Surprisal by Layer {model_name}")
+    fig.write_image(f"scripts/{filename}.png")
+    fig.show()
+
+    # fig = create_heatmap(source_layers, target_layers, values, title=f"Token Identity: Surprisal by Layer {model_name}")
+    # # Save as png
+    # fig.write_image(f"scripts/{filename}.png")
+    # fig.show()
+
+
+if __name__ == "__main__":
+    app()
