c50546c5073de7741f336d0c5d900b2a10c2b082
fergus <fergusfettes@gmail.com>
2024-02-25 21:24:55 +0530

    made the token identity script into a script

--- scripts/token_identity.py
+++ scripts/token_identity.py
@@ -6,11 +6,6 @@ from datasets import load_dataset
 dataset = load_dataset('oscar-corpus/OSCAR-2201', 'en', split='train', streaming=True)
 shuffled_dataset = dataset.shuffle(seed=42, buffer_size=10_000)
 
-samples = []
-for example in shuffled_dataset.take(5):
-    samples.append(example['text'])
-
-
 model_names = {
     "llamatiny": "TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T",
     "llama": "meta-llama/Llama-2-13b-hf",
@@ -22,43 +17,63 @@ model_names = {
 }
 
 
-# Trim the samples to the first 300 characters
-samples = [sample[:1000] for sample in samples]
+def main(model_name, n_samples=5, full=False):
+    samples = []
+    for example in shuffled_dataset.take(n_samples):
+        samples.append(example['text'])
 
-# Make sure it ends on a space
-samples = [sample[:sample.rfind(' ')] for sample in samples]
+    # Trim the samples to the first 300 characters
+    samples = [sample[:1000] for sample in samples]
 
-# Strip the spaces
-samples = [sample.strip() for sample in samples]
+    # Make sure it ends on a space
+    samples = [sample[:sample.rfind(' ')] for sample in samples]
 
-surprisals = []
-model_name = "gpt2"
-ti = TokenIdentity("", model_names[model_name])
-for prompt in samples:
-    ti.filename = f"token_identity_{model_name}_{prompt.replace(' ', '')[:10]}"
-    ti.patchscope.source.prompt = prompt
-    ti.run(
-        range(ti.patchscope.n_layers_source),
-        # range(ti.patchscope.n_layers_target)
-    ).compute_surprisal().visualize()
-    surprisals.append(ti.surprisal)
+    # Strip the spaces
+    samples = [sample.strip() for sample in samples]
 
-# Average the surprisals, calculate the standard deviation and plot with plotly
-import numpy as np
+    ti = TokenIdentity("", model_names[model_name])
 
-if len(surprisals[0].shape) == 1:
-    # Its a single set of layers
-    mean_surprisal = np.mean(surprisals, axis=0)
-    std_surprisal = np.std(surprisals, axis=0)
+    source_layers = range(ti.patchscope.n_layers_source // 3)
+    target_layers = range(ti.patchscope.n_layers_target // 3)
 
-    fig = plot_surprisal(ti.source_layers, mean_surprisal, std_surprisal, "Surprisal of the first 1000 characters of 10 random samples from the OSCAR corpus")
-    fig.write_html(f"mean_surprisal_heatmap_{model_names[model_name]}_{len(samples)}_samples.html")
-    fig.show()
+    surprisals = []
+    for prompt in samples:
+        ti.filename = f"token_identity_{model_name}_{prompt.replace(' ', '')[:10]}"
+        ti.patchscope.source.prompt = prompt
+        ti.run(
+            source_layers=source_layers,
+            target_layers=target_layers if full else None
+        ).compute_surprisal().visualize()
+        surprisals.append(ti.surprisal)
 
-elif len(surprisals[0].shape) == 2:
-    # Its a set of layers for each token, meaning a heatmap. We dont botther with the std
-    mean_surprisal = np.mean(surprisals, axis=0)
+    # Average the surprisals, calculate the standard deviation and plot with plotly
+    import numpy as np
 
-    fig = create_heatmap(ti.source_layers, ti.target_layers, mean_surprisal, "Surprisal of the first 1000 characters of 10 random samples from the OSCAR corpus")
-    fig.write_html(f"mean_surprisal_heatmap_{model_names[model_name]}_{len(samples)}_samples.html")
-    fig.show()
+    if len(surprisals[0].shape) == 1:
+        # Its a single set of layers
+        mean_surprisal = np.mean(surprisals, axis=0)
+        std_surprisal = np.std(surprisals, axis=0)
+
+        fig = plot_surprisal(ti.source_layers, mean_surprisal, std_surprisal, "Surprisal of the first 1000 characters of 10 random samples from the OSCAR corpus")
+        fig.write_html(f"mean_surprisal_heatmap_{model_names[model_name]}_{len(samples)}_samples.html")
+        fig.show()
+
+    elif len(surprisals[0].shape) == 2:
+        # Its a set of layers for each token, meaning a heatmap. We dont botther with the std
+        mean_surprisal = np.mean(surprisals, axis=0)
+
+        fig = create_heatmap(ti.source_layers, ti.target_layers, mean_surprisal, "Surprisal of the first 1000 characters of 10 random samples from the OSCAR corpus")
+        fig.write_html(f"mean_surprisal_heatmap_{model_names[model_name]}_{len(samples)}_samples.html")
+        fig.show()
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Calculate the surprisal of a set of samples using a model")
+    parser.add_argument("model_name", type=str, help="The name of the model to use")
+    parser.add_argument("--n", type=int, default=5, help="The number of samples to average over")
+    parser.add_argument("--full", action="store_false", help="Whether to run over all layers or pairs")
+    args = parser.parse_args()
+
+    main(args.model_name)
