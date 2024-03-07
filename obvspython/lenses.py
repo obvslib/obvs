3164d49ea3a9408d78d17937c97f65c64a329b0e
Lukas Linauer <lukas@datenvorsprung.at>
2024-03-07 08:43:12 +0100

    Rename LogitLens -> PatchscopeLogitLens, do not inherit from Patchscope class, re-structure to the run/visualize schema

--- obvspython/lenses.py
+++ obvspython/lenses.py
@@ -1,17 +1,18 @@
 """
 lenses.py
 
-Implementation of some widely-known lenses in the Patchscope framework
+Implementation of some widely-known lenses
 """
 
 from typing import List
 import torch
+from plotly.graph_objects import Figure
 from obvspython.logging import logger
 from obvspython.vis import create_annotated_heatmap
 from obvspython.patchscope import SourceContext, TargetContext, Patchscope
 
 
-class LogitLens(Patchscope):
+class PatchscopeLogitLens:
     """ Implementation of logit-lens in patchscope framework.
         The logit-lens is defined in the patchscope framework as follows:
         S = T   (source prompt = target prompt)
@@ -45,16 +46,15 @@ class LogitLens(Patchscope):
 
         # create TargetContext from SourceContext, as they are mostly equal
         target_context = TargetContext.from_source(source_context)
-        target_context.layer = -1  # for the logit-lens, the target layer is always the last layer
+        # for the logit-lens, the target layer is always the last layer
+        target_context.layer = -1
 
         # create Patchscope object
         self.patchscope = Patchscope(source_context, target_context)
+        self.data = {}
 
-    def create_top_token_logit_pred_heatmap(self, substring: str, layers: List[int]):
-        """ Create a heatmap of the top predicted token and its logit for each layer in layers
-            and each token in substring.
-            Based on the plot from:
-            https://www.lesswrong.com/posts/AcKRB8wDpdaN6v6ru/interpreting-gpt-the-logit-lens
+    def run(self, substring: str, layers: List[int]):
+        """ Run the logit lens for each layer in layers and each token in substring.
 
         Args:
             substring (str): Substring of the prompt for which the top prediction and logits
@@ -70,16 +70,10 @@ class LogitLens(Patchscope):
                          self.source.prompt)
             return
 
-        logits = []
-        preds = []
-        x_ticks = [f'{self.patchscope.tokenizer.decode(tok)}' for tok in substring_tokens]
-        y_ticks = [f'{self.patchscope.MODEL_SOURCE}_{self.patchscope.LAYER_SOURCE}{i}'
-                   for i in layers]
+        self.data['logits'] = {}
 
         # for each token in the substring,
         for i, layer in enumerate(layers):
-            layer_logits = []
-            layer_preds = []
             for j in range(len(substring_tokens)):
 
                 self.patchscope.source.layer = layer
@@ -87,18 +81,61 @@ class LogitLens(Patchscope):
                 self.patchscope.target.position = start_pos + j
                 self.patchscope.run()
 
-                # get the top prediction and logit
-                top_logit, top_pred_idx = torch.max(self.patchscope.logits()[start_pos + j], dim=0)
-                # convert the top_pred_idx to a word
-                top_pred = self.patchscope.tokenizer.decode(top_pred_idx)
+                self.data['logits'][(i, j)] = self.patchscope.logits()
+        self.data['substring'] = substring_tokens
+        self.data['layers'] = layers
+        self.data['start_pos'] = start_pos
 
-                layer_logits.append(top_logit.item())
-                layer_preds.append(top_pred)
+    def visualize(self, kind: str = 'top_logits_preds', file_name: str = '') -> Figure:
+        """ Visualize the logit lens results in one of the following ways:
+                top_logits_preds: Heatmap with the top predicted tokens and their logits
+        Args:
+              kind (str): The kind of visualization
+              file_name (str): If provided, save figure to a file with the given name
+                               (in the current path)
+        Returns (plotly.graph_objects.Figure):
+            The created figure
+        """
 
-            logits.append(layer_logits)
-            preds.append(layer_preds)
+        if not self.data:
+            logger.error('You need to call .run() before .visualize()!')
+            return go.Figure()
 
-        # create a heatmap with the top logits and predicted tokens
-        fig = create_annotated_heatmap(logits, preds, x_ticks, y_ticks,
-                                       title='Top predicted token and its logit')
-        fig.write_html(f'{self.model_name}_logitlens_top_logits_preds.html')
+        if kind == 'top_logits_preds':
+
+            logits = []
+            preds = []
+
+            # loop over all layers
+            for i, layer in enumerate(self.data['layers']):
+                layer_logits = []
+                layer_preds = []
+
+                # loop over every token in substring
+                for j in range(len(self.data['substring'])):
+
+                    # get the top prediction and logit
+                    top_logit, top_pred_idx = torch.max(
+                        self.data['logits'][(i, j)][self.data['start_pos'] + j], dim=0
+                    )
+                    # convert the top_pred_idx to a word
+                    top_pred = self.patchscope.tokenizer.decode(top_pred_idx)
+
+                    layer_logits.append(top_logit.item())
+                    layer_preds.append(top_pred)
+
+                logits.append(layer_logits)
+                preds.append(layer_preds)
+
+            x_ticks = [f'{self.patchscope.tokenizer.decode(tok)}'
+                       for tok in self.data['substring']]
+            y_ticks = [f'{self.patchscope.MODEL_SOURCE}_{self.patchscope.LAYER_SOURCE}{i}'
+                       for i in self.data['layers']]
+
+            # create a heatmap with the top logits and predicted tokens
+            fig = create_annotated_heatmap(logits, preds, x_ticks, y_ticks,
+                                           title='Top predicted token and its logit')
+
+        if file_name:
+            fig.write_html(f'{file_name.replace(".html", "")}.html')
+        return fig
