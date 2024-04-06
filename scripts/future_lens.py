b60d3f8e80372be89dd69ec7f1c3339314ae075c
Vy Hong <shiweistg@gmail.com>
2024-04-06 12:49:15 -0700

    Fix relative path in future_lens script

--- scripts/future_lens.py
+++ scripts/future_lens.py
@@ -1,14 +1,20 @@
+import os
 import torch
-from nnsight import LanguageModel
 from obvs.patchscope import Patchscope, SourceContext, TargetContext
 
 
 MODEL_NAME = 'EleutherAI/gpt-j-6b'
-PREFIX_PATH = "../data/processed/gptj_soft_prefix.pt"
+PREFIX_PATH = os.path.join(
+    os.path.dirname(__file__),
+    "..", "data", "processed", "gptj_soft_prefix.pt")
 DEVICE = "auto"
 
 
 def future_lens():
+    """
+    Simple script to generate the future lens at a single position.
+    """
+
     soft_prompt = torch.load(PREFIX_PATH).to(torch.float32).detach()
 
     source = SourceContext(
